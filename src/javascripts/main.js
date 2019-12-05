2ef1b11e9d3edd8e6e7e0eddc1074b1a0f08708e
Maggie Greene <maggie.greene96@gmail.com>
2019-12-05 05:07:25 -0600

    Work in Progress

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,11 +2,13 @@ import 'bootstrap';
 import '../styles/main.scss';
 import projects from './components/projects';
 
+const home = document.getElementById('homePage');
 const bio = document.getElementById('bioPage');
 const technologies = document.getElementById('technologiesPage');
 const projectsPage = document.getElementById('projectsPage');
 
 document.body.onload = () => {
+  bio.style.display = 'none';
   technologies.style.display = 'none';
   projectsPage.style.display = 'none';
 };
@@ -15,16 +17,24 @@ document.body.addEventListener('click', (event) => {
   event.preventDefault();
   if (event.target.id === 'navToBio') {
     bio.style.display = '';
+    home.style.display = 'none';
     technologies.style.display = 'none';
     projectsPage.style.display = 'none';
   } else if (event.target.id === 'navToTechnologies') {
     bio.style.display = 'none';
+    home.style.display = 'none';
     technologies.style.display = '';
     projectsPage.style.display = 'none';
   } else if (event.target.id === 'navToProjects') {
     bio.style.display = 'none';
+    home.style.display = 'none';
     technologies.style.display = 'none';
     projectsPage.style.display = '';
+  } else if (event.target.id === 'navToHome') {
+    bio.style.display = 'none';
+    home.style.display = '';
+    technologies.style.display = 'none';
+    projectsPage.style.display = 'none';
   }
 });
 
